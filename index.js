008b9a156960e0a5bb422da81ffa8a52a05c8b60
Vikram <mamundivikram@gmail.com>
2024-04-01 19:21:47 +0530

    data binding in progress

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({
     extended:true
 }))
 
-mongoose.connect('mongodb://0.0.0.0:27017/restaurant',{ useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://0.0.0.0:27017/restaurant');
 
 var db = mongoose.connection;
 
@@ -174,6 +174,608 @@ app.get('/api/users',(req,res)=> {
 
   res.json(users);
 });
+
+
+app.get('/api/foodItems', (req, res) => {
+  foodData = [{
+    "_id": {
+      "$oid": "660a9864f663d40324a0a260"
+    },
+    "Fooditem": "Pork and Chive Dumpling",
+    "Discount": 15,
+    "Tag": "Non-veg",
+    "Oldprice": 220,
+    "Newprice": 187,
+    "img":"image3.jpeg"
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a261"
+    },
+    "Fooditem": "Chicken and Mushroom Dumpling",
+    "Discount": 15,
+    "Tag": "Non-veg",
+    "Oldprice": 250,
+    "Newprice": 213,
+    "img":"chicken.jpg"
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a262"
+    },
+    "Fooditem": "Vegetable Dumpling (Spinach, Mushroom and Tofu) ",
+    "Discount": 10,
+    "Tag": "Veg",
+    "Oldprice": 210,
+    "Newprice": 189,
+    "img":"image4.jpg"
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a263"
+    },
+    "Fooditem": "Seafood Surprise Dumpling",
+    "Discount": 10,
+    "Tag": "Non-veg",
+    "Oldprice": 350,
+    "Newprice": 315,
+    "img":"image1.jpg"
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a264"
+    },
+    "Fooditem": "Cheese Burst Dumpling",
+    "Discount": 10,
+    "Tag": "Veg",
+    "Oldprice": 180,
+    "Newprice": 162,
+    "img":"image5.avif"
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a265"
+    },
+    "Fooditem": "Crispy Pork Dumpling",
+    "Discount": 10,
+    "Tag": "Non-veg",
+    "Oldprice": 200,
+    "Newprice": 180,
+    "img":"image5.avif"
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a266"
+    },
+    "Fooditem": "Shrimp and Garlic Dumpling ",
+    "Discount": 10,
+    "Tag": "Non-veg",
+    "Oldprice": 280,
+    "Newprice": 252,
+    "img":"saucedumpling.jpg"
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a267"
+    },
+    "Fooditem": "Vegetarian Potstickers",
+    "Discount": 10,
+    "Tag": "Veg",
+    "Oldprice": 190,
+    "Newprice": 171,
+    "img":"image3.jpeg"
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a268"
+    },
+    "Fooditem": "Crispy Chicken Cheese Dumpling",
+    "Discount": 10,
+    "Tag": "Non-veg",
+    "Oldprice": 300,
+    "Newprice": 270,
+    "img":"image5.avif"
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a269"
+    },
+    "Fooditem": "Chicken Dumpling Comfort Soup",
+    "Discount": 10,
+    "Tag": "Non-veg",
+    "Oldprice": 290,
+    "Newprice": 261,
+    "img":"image6.jpg"
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a26a"
+    },
+    "Fooditem": "Prawn Dumpling Broth Bliss",
+    "Discount": 10,
+    "Tag": "Non-veg",
+    "Oldprice": 310,
+    "Newprice": 279,
+    "img":"image7.jpeg"
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a26b"
+    },
+    "Fooditem": "Tofu Dumpling Zen Noodle Soup",
+    "Discount": 10,
+    "Tag": "Veg",
+    "Oldprice": 280,
+    "Newprice": 252,
+    "img":"image6.jpg"
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a26c"
+    },
+    "Fooditem": "Prawn Dumpling Harmony Stew",
+    "Discount": 10,
+    "Tag": "Non-veg",
+    "Oldprice": 310,
+    "Newprice": 279,
+    "img":"image7.jpeg"
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a26d"
+    },
+    "Fooditem": "Chicken and Prawn Dumpling Miso Soup",
+    "Discount": 10,
+    "Tag": "Non-veg",
+    "Oldprice": 320,
+    "Newprice": 288,
+    "img":"image7.jpeg"
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a26e"
+    },
+    "Fooditem": "Dragon's Breath Chicken Dumpling Hot Pot",
+    "Discount": 10,
+    "Tag": "Non-veg",
+    "Oldprice": 280,
+    "Newprice": 252,
+    "img":"image6.jpg"
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a26f"
+    },
+    "Fooditem": "Dumpling Delight Platter (Chicken, Prawn, Tofu, Mushroom)",
+    "Discount": 5,
+    "Tag": "Non-veg",
+    "Oldprice": 380,
+    "Newprice": 361,
+    "img":"image8.jpg"
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a270"
+    },
+    "Fooditem": "Golden Crisp Dumpling Basket",
+    "Discount": 5,
+    "Tag": "Non-veg/Veg",
+    "Oldprice": 400,
+    "Newprice": 380,
+    "img":"image9.jpg"
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a271"
+    },
+    "Fooditem": "Blossom Dumpling Extravaganza",
+    "Discount": 5,
+    "Tag": "Non-veg/Veg",
+    "Oldprice": 390,
+    "Newprice": 371,
+    "img":"image10.jpg"
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a272"
+    },
+    "Fooditem": "Dumpling Noodle Soup (with choice of dumplings)",
+    "Discount": 5,
+    "Tag": "Non-veg/Veg",
+    "Oldprice": 400,
+    "Newprice": 380,
+    "img":"image6.jpg"
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a273"
+    },
+    "Fooditem": "Stir-Fried Noodles with Dumplings and Vegetables",
+    "Discount": 5,
+    "Tag": "Non-veg/Veg",
+    "Oldprice": 310,
+    "Newprice": 295,
+    "img":"stirfried.jpg"
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a274"
+    },
+    "Fooditem": "Teriyaki Chicken Dumpling Rice Bowl",
+    "Discount": 10,
+    "Tag": "Non-veg",
+    "Oldprice": 290,
+    "Newprice": 261,
+    "img":"nonvegricebowl.jpeg"
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a275"
+    },
+    "Fooditem": "Beef Bulgogi Dumpling Rice Bowl",
+    "Discount": 10,
+    "Tag": "Non-veg",
+    "Oldprice": 210,
+    "Newprice": 189,
+    "img":"nonvegricebowl.jpeg"
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a276"
+    },
+    "Fooditem": "Tofu and Vegetable Dumpling Rice Bowl",
+    "Discount": 10,
+    "Tag": "Veg",
+    "Oldprice": 180,
+    "Newprice": 162,
+    "img":"vegricebowl.webp"
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a277"
+    },
+    "Fooditem": "Bok Choy Wrapped Dumpling",
+    "Discount": 10,
+    "Tag": "Non-veg/Veg",
+    "Oldprice": 210,
+    "Newprice": 189,
+    "img":"image11.jpg"
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a278"
+    },
+    "Fooditem": "Kung Pao Dumplings",
+    "Discount": 10,
+    "Tag": "Non-veg",
+    "Oldprice": 230,
+    "Newprice": 207,
+    "img":"chicken.jpg"
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a279"
+    },
+    "Fooditem": "Sweet and Sour Dumplings",
+    "Discount": 10,
+    "Tag": "Non-veg/Veg",
+    "Oldprice": 230,
+    "Newprice": 207,
+    "img":"image6.jpg"
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a27a"
+    },
+    "Fooditem": "Spicy Garlic Dumpling Stir-Fry",
+    "Discount": 10,
+    "Tag": "Veg",
+    "Oldprice": 170,
+    "Newprice": 153,
+    "img":"stirfried.jpg"
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a27b"
+    },
+    "Fooditem": "Dumplings in Spicy Sichuan Sauce",
+    "Discount": 10,
+    "Tag": "Non-veg/Veg",
+    "Oldprice": 190,
+    "Newprice": 171,
+    "img":"saucedumpling.jpg"
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a27c"
+    },
+    "Fooditem": "Dumplings in Black Bean Sauce",
+    "Discount": 10,
+    "Tag": "Non-veg/Veg",
+    "Oldprice": 220,
+    "Newprice": 198,
+    "img":"blackbean.jpg"
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a27d"
+    },
+    "Fooditem": "Dumplings in Chili Oil",
+    "Discount": 15,
+    "Tag": "Non-veg/Veg",
+    "Oldprice": 190,
+    "Newprice": 162,
+    "img":"saucedumpling.jpg"
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a27e"
+    },
+    "Fooditem": "Asian Slaw: Shredded cabbage, carrots, and sesame dressing.",
+    "Discount": 15,
+    "Tag": "Veg",
+    "Oldprice": 100,
+    "Newprice": 85,
+    "img":"salad.jpg"
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a27f"
+    },
+    "Fooditem": "Steamed Greens: Bok Choy or Chinese Broccoli with Garlic Sauce.",
+    "Discount": 15,
+    "Tag": "Veg",
+    "Oldprice": 100,
+    "Newprice": 85
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a280"
+    },
+    "Fooditem": "Fried Rice: Classic, Vegetable, or Shrimp Fried Rice.",
+    "Discount": 15,
+    "Tag": "Non-veg/Veg",
+    "Oldprice": 180,
+    "Newprice": 153
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a281"
+    },
+    "Fooditem": "Sauces: Soy sauce, chili oil, vinegar, and garlic sauce for dipping.",
+    "Discount": 0,
+    "Tag": "Veg",
+    "Oldprice": 20,
+    "Newprice": 20
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a282"
+    },
+    "Fooditem": "Dragon Chicken",
+    "Discount": 15,
+    "Tag": "Non-veg",
+    "Oldprice": 190,
+    "Newprice": 162
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a283"
+    },
+    "Fooditem": "Chili Prawn",
+    "Discount": 15,
+    "Tag": "Non-veg",
+    "Oldprice": 250,
+    "Newprice": 213
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a284"
+    },
+    "Fooditem": "Chili Mushroom",
+    "Discount": 15,
+    "Tag": "Veg",
+    "Oldprice": 160,
+    "Newprice": 136
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a285"
+    },
+    "Fooditem": "Chili Baby Corn and Tofu",
+    "Discount": 15,
+    "Tag": "Veg",
+    "Oldprice": 210,
+    "Newprice": 179
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a286"
+    },
+    "Fooditem": "Red Bean Paste Dumpling",
+    "Discount": 15,
+    "Tag": "Veg",
+    "Oldprice": 170,
+    "Newprice": 145
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a287"
+    },
+    "Fooditem": "Sesame Ball Dumpling",
+    "Discount": 15,
+    "Tag": "Veg",
+    "Oldprice": 170,
+    "Newprice": 145
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a288"
+    },
+    "Fooditem": "Pineapple and Coconut Dumpling",
+    "Discount": 15,
+    "Tag": "Veg",
+    "Oldprice": 170,
+    "Newprice": 145
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a289"
+    },
+    "Fooditem": "Chocolate Dumpling",
+    "Discount": 15,
+    "Tag": "Veg",
+    "Oldprice": 170,
+    "Newprice": 145
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a28a"
+    },
+    "Fooditem": "Mango Sticky Rice",
+    "Discount": 15,
+    "Tag": "Veg",
+    "Oldprice": 130,
+    "Newprice": 111
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a28b"
+    },
+    "Fooditem": "Fried Ice Cream",
+    "Discount": 0,
+    "Tag": "Veg",
+    "Oldprice": 110,
+    "Newprice": 110
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a28c"
+    },
+    "Fooditem": "Rolled Ice Cream",
+    "Discount": 0,
+    "Tag": "Veg",
+    "Oldprice": 110,
+    "Newprice": 110
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a28d"
+    },
+    "Fooditem": "Cheese Cake",
+    "Discount": 0,
+    "Tag": "Veg",
+    "Oldprice": 110,
+    "Newprice": 110,
+    "img":"cake.jpg"
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a28e"
+    },
+    "Fooditem": "Mango Milkshake",
+    "Discount": 0,
+    "Tag": "Veg",
+    "Oldprice": 90,
+    "Newprice": 90,
+    "img":"milkshake.jpg"
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a28f"
+    },
+    "Fooditem": "Kiwi and Passion Fruit Shake",
+    "Discount": 0,
+    "Tag": "Veg",
+    "Oldprice": 90,
+    "Newprice": 90,
+    "img":"milkshake.jpg"
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a290"
+    },
+    "Fooditem": "Bubble Tea: Classic Milk Tea, Taro, Mango, or Matcha flavors.",
+    "Discount": 0,
+    "Tag": "Veg",
+    "Oldprice": 80,
+    "Newprice": 80,
+    "img":"bubbletea.webp"
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a291"
+    },
+    "Fooditem": " Lychee Martini",
+    "Discount": 0,
+    "Tag": "Veg",
+    "Oldprice": 90,
+    "Newprice": 90,
+    "img":"mojito.webp"
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a292"
+    },
+    "Fooditem": "Sake Mojito",
+    "Discount": 0,
+    "Tag": "Veg",
+    "Oldprice": 90,
+    "Newprice": 90,
+    "img":"mojito.webp"
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a294"
+    },
+    "Fooditem": "Jasmine Tea",
+    "Discount": 0,
+    "Tag": "Veg",
+    "Oldprice": 70,
+    "Newprice": 70,
+    "img":"tea.jpg"
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a295"
+    },
+    "Fooditem": "Oolong Tea",
+    "Discount": 0,
+    "Tag": "Veg",
+    "Oldprice": 70,
+    "Newprice": 70,
+    "img":"tea.jpg"
+  },
+  {
+    "_id": {
+      "$oid": "660a9864f663d40324a0a296"
+    },
+    "Fooditem": "Cold Coffee",
+    "Discount": 0,
+    "Tag": "Veg",
+    "Oldprice": 90,
+    "Newprice": 90,
+    "img":"coldcoffee.jpg"
+  }]
+
+    // Log fetched data
+    console.log('Fetched foodItems:', foodData);
+    console.log('foodItems fetched')
+
+
+    // Send the fetched foodItems as JSON response
+    res.json(foodData);
+  });
+
+
+
+    // .sort({ createdAt : -1 })
+    // .toArray((err, foods) => {
+    //   if (err) throw err;
+    //   print(foods)
+    //   res.json(foods);
+    // });
+
 
 app.get('/login', (req, res) => {
     // Render the login page
--- public/homeScript.js
+++ public/homeScript.js
@@ -2,6 +2,8 @@
 //     window.open("login.html", "_blank");
 //   }
 
+// const { response } = require("express");
+
 // const cardData = [
 //   {
 //     title: "hello",
@@ -30,6 +32,113 @@
 // }
 
 // postMethods()
+
+fetch('/api/foodItems').then(response => response.json()).then(food => {
+    console.log('food fetched');
+
+    const menuContainer = document.querySelector(".food-menu-list");
+
+    const postMethods = () => {
+        food.map((postData) => {
+        console.log(postData);
+        var newEle = document.createElement('li')
+        newEle.innerHTML =
+        `
+            <div class="food-menu-card">
+                <div class="card-banner">
+                <img src="./assets/food/${postData.img}" width="300" height="300" loading="lazy"
+                alt="Fried Chicken Unlimited" class="w-100">
+                <div class="badge">-${postData.Discount}%</div>
+
+                <button class="btn food-menu-btn">Order Now</button>
+            </div>
+
+            <div class="wrapper">
+                <p class="category">${postData.Tag}</p>
+
+                <div class="rating-wrapper">
+                <ion-icon name="star"></ion-icon>
+                <ion-icon name="star"></ion-icon>
+                <ion-icon name="star"></ion-icon>
+                <ion-icon name="star"></ion-icon>
+                <ion-icon name="star"></ion-icon>
+                </div>
+            </div>
+
+            <h3 class="h3 card-title">${postData.Fooditem}</h3>
+
+            <div class="price-wrapper">
+
+                <p class="price-text">Price:</p>
+
+                <data class="price">Rs. ${postData.Newprice}</data>
+
+                <del class="del" value="69.00">Rs. ${postData.Oldprice}</del>
+
+            </div>
+
+            </div>
+        `;
+
+        menuContainer.append(newEle);
+        // const newEle = document.createElement('div');
+        // newEle.classList.add('card');
+        // newEle.innerHTML = `
+        // <p> Hello World ${postData.title} </p>
+        // <p> New Element </p>
+        // `
+        // postcontainer.appendChild(newEle);
+        });
+    }
+
+    postMethods()
+
+
+
+
+
+
+
+    // <li>
+    //           <div class="food-menu-card">
+
+    //             <div class="card-banner">
+    //               <img src="./assets/images/food-menu-1.png" width="300" height="300" loading="lazy"
+    //                 alt="Fried Chicken Unlimited" class="w-100">
+
+    //               <div class="badge">-15%</div>
+
+    //               <button class="btn food-menu-btn">Order Now</button>
+    //             </div>
+
+    //             <div class="wrapper">
+    //               <p class="category">Chicken</p>
+
+    //               <div class="rating-wrapper">
+    //                 <ion-icon name="star"></ion-icon>
+    //                 <ion-icon name="star"></ion-icon>
+    //                 <ion-icon name="star"></ion-icon>
+    //                 <ion-icon name="star"></ion-icon>
+    //                 <ion-icon name="star"></ion-icon>
+    //               </div>
+    //             </div>
+
+    //             <h3 class="h3 card-title">Fried Chicken Unlimited</h3>
+
+    //             <div class="price-wrapper">
+
+    //               <p class="price-text">Price:</p>
+
+    //               <data class="price">$49.00</data>
+
+    //               <del class="del" value="69.00">$69.00</del>
+
+    //             </div>
+
+    //           </div>
+    //         </li>
+});
+
 
 fetch('/api/users').then(response => response.json()).then(user => {
     console.log('data Feteched');
