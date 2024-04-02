dc8b4a52a4086b9fc350a717249442ac5db9d293
Vikram <mamundivikram@gmail.com>
2024-04-02 13:34:39 +0530

    img change

--- public/loginScript.js
+++ public/loginScript.js
@@ -7,47 +7,4 @@ function signincheck() {
     var email = document.getElementById('email').value;
     console.log("userData= "+ email)
     document.cookie = "userData="+ email + "; expires=Thu, 31 Dec 2037 23:59:59 UTC; path=/";
-    // fetch('/LoginAlert').then(response => response.json()).then(data => {
-        // console.log(data);
-        // handleMessage(data.message);
-
-        // if(data.type == 'login') {
-        //     fetch('/incorrectPass').then(response => response.json()).then(data => {
-        //         console.log(data);
-        //         handleMessage(data.message);
-        //     });
-        // }
-
-        // if(data.type == 'signup') {
-        //     fetch('/LoginAlert').then(response => response.json()).then(data => {
-        //         console.log(data);
-        //         handleMessage(data.message);
-        //     });
-        // }
-
-        // const menuContainer = document.querySelector(".error");
-
-        // const postMethods = () => {
-        //     food.map((postData) => {
-        //     // console.log(postData);
-        //     var newEle = document.createElement('h5')
-        //     newEle.innerHTML = ` user already registed !!`;
-
-        //     menuContainer.append(newEle);
-        //     // const newEle = document.createElement('div');
-        //     // newEle.classList.add('card');
-        //     // newEle.innerHTML = `
-        //     // <p> Hello World ${postData.title} </p>
-        //     // <p> New Element </p>
-        //     // `
-        //     // postcontainer.appendChild(newEle);
-        //     });
-        // }
-
-        // postMethods()
-
-        // document.getElementById("error").innerHTML = `
-        // <h3> User already exist </h3>
-        // `;
-    // });
 }
